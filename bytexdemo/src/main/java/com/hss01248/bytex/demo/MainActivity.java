a87c53b2448b5aa95afdc2fc1647bf7d0d1dbc87
hss01248 <hss012489@gmail.com>
2022-08-02 10:44:10 +0800

    test

---
+++ bytexdemo/src/main/java/com/hss01248/bytex/demo/MainActivity.java
@@ -0,0 +1,12 @@
+package com.hss01248.bytex.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @Despciption todo
+ * @Author hss
+ * @Date 26/07/2022 11:13
+ * @Version 1.0
+ */
+public class MainActivity extends AppCompatActivity {
+}
